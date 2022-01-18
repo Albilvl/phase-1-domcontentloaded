f36d1b31bb2c575783c415ae39f526a06625a67a
Al Bilal <bil.bank@yahoo.com>
2022-01-18 13:52:30 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener(`DOMContentLoaded`,()=>{
+    const targeter = document.querySelector(`#text`)
+    targeter.textContent = `This is really cool!`
+})
